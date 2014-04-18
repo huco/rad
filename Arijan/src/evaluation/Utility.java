f2cb6dcd7fc4c40687153ec94a953cf469a676b1
Josip <Josip@josips-mbp.dummy.porta.siemens.net>
2014-04-18 22:14:55 +0200

    distance calculation

--- Arijan/src/evaluation/Utility.java
+++ Arijan/src/evaluation/Utility.java
@@ -4,8 +4,17 @@ import data.model.Location;
 
 public class Utility {
 	
-	public static float calculateDistane(Location source, Location destination){
-		return 0f;
+	public static double calculateDistane(Location source, Location destination){
+		return Math.sqrt(longDelta(source, destination) * longDelta(source, destination) + latDelta(source, destination)
+				* latDelta(source, destination));
+	}
+	
+	private static double longDelta(Location source, Location destination){
+		return source.getLongitude() - destination.getLatitude();
+	}
+	
+	private static double latDelta(Location source, Location destination){
+		return source.getLatitude() - destination.getLatitude();
 	}
 
 }
